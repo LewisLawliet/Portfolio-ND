44054d0e82f4152575ad1728d78da63d6428d90c
Nadir Djebbar <nadirdjebbar@gmail.com>
2020-09-26 11:25:08 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
 
-ReactDom.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDom.render(<BrowserRouter></BrowserRouter>, document.getElementById('root'));
