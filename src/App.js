27f50e16378371afe35cd029462ca1eb7549f0da
Nadir Djebbar <nadirdjebbar@gmail.com>
2020-09-26 23:05:26 +0200

    test

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ class App extends React.Component {
             <HeaderNav />
                 <Switch>
                     <Route exact path="/" component={Home} />                    
-                    <Route path="/competences" component={Competences} />
-                    <Route path="/centres-d'-interet" component={CentresInterets} />
-                    <Route path="/portfolio" component={Portfolio} />
+                    <Route path="/competences/index.html" component={Competences} />
+                    <Route path="/centres-d'-interet/index.html" component={CentresInterets} />
+                    <Route path="/portfolio/index.html" component={Portfolio} />
                 </Switch>
                 <Footer />
              </div>
