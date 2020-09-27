1c8d4d6dd7559e44516508be2aaf1175920f8bd7
Nadir Djebbar <nadirdjebbar@gmail.com>
2020-09-27 15:41:56 +0200

    correction bug

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 
 
-ReactDom.render(<React.StrictMode><App /><React.StrictMode>, document.getElementById('root'));
+ReactDom.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
